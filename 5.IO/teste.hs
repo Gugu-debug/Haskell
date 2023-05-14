0ebd1553eb74ea60769fee66a63dac1b0d7edda7
Gustavo Caldas <luiz.caldas@dcomp.ufs.br>
2023-05-14 02:51:02 -0300

    Iniciando 5.IO com funcoes de leitura e edicao de arquivo

--- 5.IO/teste.hs
+++ 5.IO/teste.hs
@@ -7,12 +7,14 @@
 -- data IOMode = ReadMode | WriteMode | AppendMode | ReadWriteMode
 
 import System.IO -- Biblioteca para utilizar funcoes de arquivos
-
+import Data.Char -- Biblioteca para utilizar funcoes de caracteres
 
 main :: IO ()
 main = do
     putStr ("Digite um caminho: ")
     caminho <- getLine
-    arquivo <- openFile caminho AppendMode
-    hPutStrLn arquivo "Teste 2"
+    arquivo <- openFile caminho ReadMode
+    conteudo <- hGetContents arquivo
+    let edicao = map toUpper conteudo
+    putStrLn edicao
     hClose arquivo
